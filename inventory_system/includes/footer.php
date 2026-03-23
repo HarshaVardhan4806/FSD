655fa63eeab3bed135f11f3484b489647e6121d0
Harsha Vardhan Samala <176209399+HarshaVardhan4806@users.noreply.github.com>
2026-03-23 14:54:05 +0530

    Add files via upload

---
+++ inventory_system/includes/footer.php
@@ -0,0 +1,3 @@
+<footer style="text-align:center;padding:20px;">
+© 2026 Inventory System
+</footer>
---
+++ inventory_system/includes/header.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/style.css">
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<div class="navbar">
+<h2 style="color:#ff9900;">InventoryPro</h2>
+<a href="logout.php" style="color:white;">Logout</a>
+</div>
+
+<div class="menu">
+<a href="dashboard.php">Dashboard</a>
+<a href="products.php">Products</a>
+<a href="stock.php">Stock</a>
+<a href="suppliers.php">Suppliers</a>
+<a href="reports.php">Reports</a>
+</div>
